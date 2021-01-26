0a46ae597539d2532ca3dd095cecc834c739849c
Abubaker Tagelsir <abubaker.tagelsir@gmail.com>
2021-01-26 11:18:16 +0400

    link auth and user routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 const towersRoutes = require('./towers');
+const authRoutes = require('./auth');
+const userRoutes = require('./users');
 
 router.use('/towers', towersRoutes);
 
+router.use('/auth', authRoutes);
+
+router.use('/user', userRoutes);
 
 module.exports = router;
